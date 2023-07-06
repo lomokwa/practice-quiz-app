d9915aaf55681adf5cb18dd673ab37744641421a
Lorenzo Mokwa <lomokwa@Lorenzos-MacBook-Air.local>
2023-07-06 10:08:04 -0400

    added pages, navbar, and footer

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,9 @@
+import AppLayout from "../layout/AppLayout";
+
+export default function Error404() {
+  return (
+    <AppLayout>
+      <h2>Error 404</h2>
+    </AppLayout>
+  )
+}
---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,11 @@
+import AppLayout from "../layout/AppLayout";
+
+export default function Quiz() {
+
+  return(
+    <AppLayout>
+      <h2>Quiz Page</h2>
+    </AppLayout>
+  )
+  
+}
